3b6162e41af7608bc113795ff6b7af1cc16d9303
sectore <info@websector.de>
2016-06-08 22:43:29 +0200

    exercise 1: migrate to 0.17

--- Main.elm
+++ Main.elm
@@ -1,4 +1,4 @@
-module Main (..) where
+module Main exposing (..)
 
 -- --------------------------
 -- Exercise 1:
@@ -16,37 +16,35 @@ module Main (..) where
 -- 2. Compile the resulting JS file with the Closure Compiler in ADVANCED mode
 --
 -- https://developers.google.com/closure/compiler/
--- 
+--
 -- --------------------------
 -- Exercise 3 (optional):
 -- --------------------------
 -- Create an outgoing port that exposes the current forecast. Create an
 -- index.html and embed the elm application in fullscreen mode. Log the
--- received forecast-data to the browser console.   
+-- received forecast-data to the browser console.
 -- Start elm reactor and check if everything works as exptected.
--- 
+--
 -- http://guide.elm-lang.org/interop/javascript.html
 
-
+import Html.App as Html
 import Html exposing (..)
 import Html.Events exposing (onClick)
-import Signal exposing (..)
-import StartApp.Simple as StartApp
 
 
 -- MODEL
 
 
 type alias Weather =
-  { day : String, max : Int, min : Int, description : String }
+    { day : String, max : Int, min : Int, description : String }
 
 
 type alias City =
-  String
+    String
 
 
 type alias Model =
-  { city : City, forecast : List Weather }
+    { city : City, forecast : List Weather }
 
 
 
@@ -55,30 +53,30 @@ type alias Model =
 
 forecast_hh : Model
 forecast_hh =
-  { city = "Hamburg"
-  , forecast =
-      [ { day = "TUE", max = 19, min = 8, description = "Light Rain" }
-      , { day = "WED", max = 15, min = 4, description = "Mostly Sunny" }
-      , { day = "THU", max = 17, min = 6, description = "Sunny" }
-      , { day = "FRI", max = 20, min = 10, description = "Sunny" }
-      , { day = "SAT", max = 22, min = 11, description = "Sunny" }
-      , { day = "SUN", max = 22, min = 12, description = "Sunny" }
-      ]
-  }
+    { city = "Hamburg"
+    , forecast =
+        [ { day = "TUE", max = 19, min = 8, description = "Light Rain" }
+        , { day = "WED", max = 15, min = 4, description = "Mostly Sunny" }
+        , { day = "THU", max = 17, min = 6, description = "Sunny" }
+        , { day = "FRI", max = 20, min = 10, description = "Sunny" }
+        , { day = "SAT", max = 22, min = 11, description = "Sunny" }
+        , { day = "SUN", max = 22, min = 12, description = "Sunny" }
+        ]
+    }
 
 
 forecast_berlin : Model
 forecast_berlin =
-  { city = "Berlin" 
-  , forecast = 
-    [ { day = "TUE", max = 18, min = 7, description = "Partly Cloudy" }
-    , { day = "WED", max = 16, min = 4, description = "Cloudy" }
-    , { day = "THU", max = 19, min = 6, description = "Sunny" }
-    , { day = "FRI", max = 21, min = 10, description = "Mostly Sunny" }
-    , { day = "SAT", max = 23, min = 11, description = "Mostly Sunny" }
-    , { day = "SUN", max = 24, min = 12, description = "Mostly Sunny" }
-    ]
-  }
+    { city = "Berlin"
+    , forecast =
+        [ { day = "TUE", max = 18, min = 7, description = "Partly Cloudy" }
+        , { day = "WED", max = 16, min = 4, description = "Cloudy" }
+        , { day = "THU", max = 19, min = 6, description = "Sunny" }
+        , { day = "FRI", max = 21, min = 10, description = "Mostly Sunny" }
+        , { day = "SAT", max = 23, min = 11, description = "Mostly Sunny" }
+        , { day = "SUN", max = 24, min = 12, description = "Mostly Sunny" }
+        ]
+    }
 
 
 
@@ -87,103 +85,95 @@ forecast_berlin =
 
 initalModel : Model
 initalModel =
-  forecast_hh
+    forecast_hh
 
 
 
 -- UPDATE
 
 
-type Action
-  = Reset
-  | Show Model
-  | Toggle
+type Msg
+    = Reset
+    | Show Model
+    | Toggle
 
 
-update : Action -> Model -> Model
-update action model =
-  case action of
-    Reset ->
-      initalModel
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Reset ->
+            initalModel
 
-    Show forecast ->
-      forecast
+        Show forecast ->
+            forecast
 
-    Toggle ->
-      if model == forecast_hh then 
-        forecast_berlin 
-      else 
-        forecast_hh
+        Toggle ->
+            if model == forecast_hh then
+                forecast_berlin
+            else
+                forecast_hh
 
 
 
 -- VIEW
 
 
-forecastListView : List Weather -> Html
+forecastListView : List Weather -> Html msg
 forecastListView forecast =
-  let
-    -- table header
-    header =
-      tr
-        []
-        [ th [] [ text "Day" ]
-        , th [] [ text "Max" ]
-        , th [] [ text "Min" ]
-        , th [] [ text "Detail" ]
+    let
+        -- table header
+        header =
+            tr []
+                [ th [] [ text "Day" ]
+                , th [] [ text "Max" ]
+                , th [] [ text "Min" ]
+                , th [] [ text "Detail" ]
+                ]
+
+        -- table content
+        list =
+            case forecast of
+                [] ->
+                    []
+
+                detail ->
+                    List.map forecastDetailView detail
+    in
+        table []
+            [ thead []
+                [ header ]
+            , tbody []
+                list
+            ]
+
+
+forecastDetailView : Weather -> Html msg
+forecastDetailView detail =
+    tr []
+        [ td [] [ text detail.day ]
+        , td [] [ text (toString detail.max ++ "°C") ]
+        , td [] [ text (toString detail.min ++ "°C") ]
+        , td [] [ text detail.description ]
         ]
 
-    -- table content
-    list =
-      case forecast of
-        [] ->
-          []
 
-        detail ->
-          List.map forecastDetailView detail
-  in
-    table
-      []
-      [ thead
-          []
-          [ header ]
-      , tbody
-          []
-          list
-      ]
-
-
-forecastDetailView : Weather -> Html
-forecastDetailView detail =
-  tr
-    []
-    [ td [] [ text detail.day ]
-    , td [] [ text (toString detail.max ++ "°C") ]
-    , td [] [ text (toString detail.min ++ "°C") ]
-    , td [] [ text detail.description ]
-    ]
-
-
-cityView : City -> Html
+cityView : City -> Html msg
 cityView city =
-  h1 [] [ text city ]
+    h1 [] [ text city ]
 
 
-view : Address Action -> Model -> Html
-view address model =
-  div
-    []
-    [ button
-        [ onClick address Toggle ]
-        [ text "Toggle" ]
-    , button
-        [ onClick address Reset ]
-        [ text "Reset" ]
-    , cityView model.city
-    , forecastListView model.forecast
-    ]
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Toggle ]
+            [ text "Toggle" ]
+        , button [ onClick Reset ]
+            [ text "Reset" ]
+        , cityView model.city
+        , forecastListView model.forecast
+        ]
 
 
-main : Signal Html.Html
+main : Program Never
 main =
-  StartApp.start { model = initalModel, view = view, update = update }
+    Html.beginnerProgram { model = initalModel, view = view, update = update }
